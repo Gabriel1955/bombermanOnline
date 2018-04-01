16af36ebc7e11408b4cc836da4a4c160a5598066
DESKTOP-6TG1GHO\Jefferson Farias <jefferson.fariasm@gmail.com>
2018-04-01 19:17:49 -0300

    Enviar

--- Bomb.js
+++ Bomb.js
@@ -1,10 +1,10 @@
 bomb = {
     height: 20,
     width: 20,
-    cor:"#0",
+    cor:"#000",
     
     desenha:function(){
         ctx.fillStyle = this.cor;
-        ctx.fillRect =(myPosition_X,myPosition_Y, this.width,this.height);
+        ctx.fillRect (myPosition_X,myPosition_Y, this.width,this.height);
     }
 }
