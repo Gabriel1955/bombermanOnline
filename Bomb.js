497e111a59af680557af76885bc913191076b162
DESKTOP-6TG1GHO\Jefferson Farias <jefferson.fariasm@gmail.com>
2018-04-02 00:09:33 -0300

    the last commit, just than not

--- Bomb.js
+++ Bomb.js
@@ -1,13 +1,4 @@
 bomb = {
-<<<<<<< HEAD
-    height: 20,
-    width: 20,
-    cor:"#000",
-    
-    desenha:function(){
-        ctx.fillStyle = this.cor;
-        ctx.fillRect (myPosition_X,myPosition_Y, this.width,this.height);
-=======
     x: 0,
     y: 30,
     height: 30,
@@ -19,6 +10,5 @@ bomb = {
         var img = new Image();
         img.src = "img/bomb.png";
         ctx.drawImage(img,this.x,this.y, this.width,this.height);
->>>>>>> 4b901e79045b76c137b84b861e411d126dfa19f9
     }
 }
